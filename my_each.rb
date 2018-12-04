adc7da961343dabb15eedb56ad97869061e622b8
Don Ward <10is0420@gmail.com>
2018-12-04 02:56:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(collection)
+  binding.pry
+  i = 0
+  
+  while i < collection.length
+  
+    yield(collection[i])
+  
+     i = i + 1
+  
+   end
+
+collection
+
 end
